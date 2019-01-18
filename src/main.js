044fa390e6bc958558c0ae6c61ee827fa7a82c9c
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-01-18 11:46:10 -0800

    set up api in function to be called by Users on click

--- src/main.js
+++ src/main.js
@@ -5,16 +5,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {UserSearch} from './search';
 
 $(document).ready(function() {
-$('#symptom-button').click(function(){
-  event.preventDefault();
-  let symptom = $('#symptom-input').val();
-  let newSearch = new UserSearch(symptom);
-  console.log(newSearch);
-})
-$('#doc-button').click(function(){
-  event.preventDefault();
-  let doctor = $('#doctor-input').val();
-  let newDocSearch = new UserSearch("N/A", doctor);
-  console.log(newDocSearch);
-})
+  $('#symptom-button').click(function(){
+    event.preventDefault();
+    let symptom = $('#symptom-input').val();
+    let newSearch = new UserSearch(symptom);
+    console.log(newSearch);
+
+    let promise = newSearch.searchSymptoms();
+
+    promise.then(function(response) {
+      body = JSON.parse(response);
+      $('#first-name').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('#last-name').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('#address')
+      $('#phone-number')
+      $('#website')
+      $('#currently-accpeting')
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
+
+  });
+  $('#doc-button').click(function(){
+    event.preventDefault();
+    let doctorFirstName = $('#doctor-first-name-input').val();
+    let doctorLastName = $('#doctor-last-name-input').val();
+    let newDocSearch = new UserSearch("N/A", doctorFirstName, doctorLastName);
+
+    let promise = newDocSearch.searchDoctors();
+
+    console.log(newDocSearch);
+  });
 });
--- src/search.js
+++ src/search.js
@@ -1,7 +1,39 @@
 export class UserSearch {
-  constructor(symptom, doctorName) {
-    this.symptom = symptom
-    this.doctorName = doctorName
+  constructor(symptom, doctorFirstName, doctorLastName) {
+    this.symptom = symptom;
+    this.doctorFirstName = doctorFirstName;
+    this.doctorLastName = doctorLastName;
+  }
 
+  searchSymptoms(){
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptom}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  searchDoctors(){
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?qfirst_name=${this.doctorFirstName}&last_name=${this.doctorLastName}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
