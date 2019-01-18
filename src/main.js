ca3df3a1abf158c354ecbd9380783cbf0a1c841b
MrDanielBennett <daniel.bennett1999@gmail.com>
2019-01-18 15:31:06 -0800

    add if statement stating that if a input with no results is inputted to return a certain message

--- src/main.js
+++ src/main.js
@@ -30,11 +30,16 @@ $(document).ready(function() {
 
     promise.then(function(response) {
        let body = JSON.parse(response);
-       for(let i = 0; i < 10; i++){
-       $('.result').append(body.data[i].practices[0].name + " " + `<br>` + body.data[i].practices[0].visit_address.street + " " + `<br>` +  body.data[i].practices[0].visit_address.city + ", " +  body.data[i].practices[0].visit_address.state + " " + `<br>` + "Phone Number: " + " " + body.data[i].practices[0].phones[0].number + " " + `<br>` + "Taking new Patients? (t/f): " +
-       // removing website because it seems no doctors have them.
-        body.data[i].practices[0].accepts_new_patients + `<br>` + `<br>`)
-     }
+       if (body.data !== []){
+         $('.doctor-info').text("There are no doctors in the Portland area that match the input.")
+       } else {
+
+         for(let i = 0; i < 10; i++){
+           $('.doctor-info').append(body.data[i].practices[0].name + " " + `<br>` + body.data[i].practices[0].visit_address.street + " " + `<br>` +  body.data[i].practices[0].visit_address.city + ", " +  body.data[i].practices[0].visit_address.state + " " + `<br>` + "Phone Number: " + " " + body.data[i].practices[0].phones[0].number + " " + `<br>` + "Taking new Patients? (t/f): " +
+           // removing website because it seems no doctors have them.
+           body.data[i].practices[0].accepts_new_patients + `<br>` + `<br>`)
+         }
+       }
     },
     function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
@@ -52,7 +57,7 @@ $(document).ready(function() {
     promise.then(function(response) {
        let body = JSON.parse(response);
        for(let i = 0; i < 10; i++){
-         $('.result').append(body.data[i].practices[0].name + " " + `<br>` + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + " " + body.data[i].practices[0].visit_address.state + " " +  body.data[i].practices[0].phones[0].number + " " + `<br>` + "Taking new Patients? (t/f): " +
+         $('.doctor-info').append(body.data[i].practices[0].name + " " + `<br>` + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + " " + body.data[i].practices[0].visit_address.state + " " +  body.data[i].practices[0].phones[0].number + " " + `<br>` + "Taking new Patients? (t/f): " +
          // removing website because it seems no doctors have them.
           body.data[i].practices[0].accepts_new_patients + `<br>` + `<br>`)
        }
